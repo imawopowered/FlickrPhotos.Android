3284d046cb69ae7e1337f82c5c44e4b5db89d0be
Ovidiu Petru Muntean <ovidiu.muntean@renfieldsoftware.com>
2023-05-10 16:36:26 +0300

    Initial commit

--- app/src/main/java/com/mindera/flickergallery/network/RetrofitService.kt
+++ app/src/main/java/com/mindera/flickergallery/network/RetrofitService.kt
@@ -1,6 +1,7 @@
 package network
 
 import com.mindera.flickergallery.model.Photo
+import com.mindera.flickergallery.model.PhotosJson
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -9,7 +10,7 @@ import utils.Constants.BASE_URL
 
 interface RetrofitService {
     @GET("?method=flickr.photos.search&api_key=9a95c68a9c6ec61104cd3967dcbb8bd3&tags=snail&page=1&format=json&nojsoncallback=1")
-    fun getAllPhotos(): Call<List<Photo>>
+    fun getAllPhotos(): Call<PhotosJson>
 
     companion object {
 
