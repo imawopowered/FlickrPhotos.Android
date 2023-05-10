e4676bca588cec340c39a883610e025ed65d71e2
Ovidiu Petru Muntean <ovidiu.muntean@renfieldsoftware.com>
2023-05-10 22:41:10 +0300

    Initial commit

--- app/src/main/java/com/mindera/flickergallery/viewmodel/PhotosViewModel.kt
+++ app/src/main/java/com/mindera/flickergallery/viewmodel/PhotosViewModel.kt
@@ -3,13 +3,16 @@ package viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.mindera.flickergallery.model.Photo
+import com.mindera.flickergallery.model.PhotosJson
 import repository.PhotosRepository
+import retrofit2.Call
 
 class PhotosViewModel(private val repository: PhotosRepository): ViewModel() {
     val photosList = MutableLiveData<List<Photo>>()
+    val photoJson = MutableLiveData<Call<PhotosJson>>()
     val errorMessage = MutableLiveData<String>()
 
     fun getAllPhotos() {
-        val response = repository.getAllPhotos()
+        return photoJson.postValue(repository.getAllPhotos())
     }
 }
