dd6c081f258b2a3f258c127d3d3a5f111bba592c
Ovidiu Petru Muntean <ovidiu.muntean@renfieldsoftware.com>
2023-05-11 11:32:54 +0300

    Initial commit

--- app/src/main/java/com/mindera/flickergallery/network/GetPhotosRetrofitService.kt
+++ app/src/main/java/com/mindera/flickergallery/network/GetPhotosRetrofitService.kt
@@ -1,6 +1,7 @@
 package network
 
 import com.mindera.flickergallery.model.PhotosJson
+import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
@@ -8,7 +9,7 @@ import utils.Constants
 
 interface GetPhotosRetrofitService {
     @GET("&api_key=9a95c68a9c6ec61104cd3967dcbb8bd3&tags=snail&page=1&format=json&nojsoncallback=1")
-    fun getAllPhotos(): PhotosJson
+    fun getAllPhotos(): Call<PhotosJson>
 
     companion object {
 
--- app/src/main/java/com/mindera/flickergallery/network/GetSizesRetrofitService.kt
+++ app/src/main/java/com/mindera/flickergallery/network/GetSizesRetrofitService.kt
@@ -1,14 +1,16 @@
 package network
 
 import com.mindera.flickergallery.model.SizesJson
+import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Query
 import utils.Constants.BASE_URL_GET_SIZES
 
 interface GetSizesRetrofitService {
-    @GET("&api_key=9a95c68a9c6ec61104cd3967dcbb8bd3&photo_id=52882160209&format=json&nojsoncallback=1")
-    fun getAllSizes(): SizesJson
+    @GET("&api_key=9a95c68a9c6ec61104cd3967dcbb8bd3&{photo_id}&format=json&nojsoncallback=1")
+    fun getAllSizes(@Query("photo_id") photoId: String): Call<SizesJson>
 
     companion object {
 
