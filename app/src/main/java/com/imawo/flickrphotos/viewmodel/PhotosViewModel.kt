f0311ddf35667a37f10f05f1ae702900625be9bb
Ovidiu Petru Muntean <ovidiu.muntean@renfieldsoftware.com>
2023-05-27 13:39:50 +0300

    Initial commit

--- app/src/main/java/com/imawo/flickrphotos/viewmodel/PhotosViewModel.kt
+++ app/src/main/java/com/imawo/flickrphotos/viewmodel/PhotosViewModel.kt
@@ -3,8 +3,10 @@ package viewmodel
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.imawo.flickrphotos.model.Photo
 import com.imawo.flickrphotos.model.PhotosJson
+import kotlinx.coroutines.launch
 import repository.PhotosRepository
 import retrofit2.Call
 import retrofit2.Callback
@@ -20,25 +22,27 @@ class PhotosViewModel(private val repository: PhotosRepository): ViewModel() {
         get() = _allPhotosLiveDataList
 
     fun getAllPhotos() {
-        val response = repository.getAllPhotos()
-        val errorMessage = MutableLiveData<String>()
+        viewModelScope.launch {
+            val response = repository.getAllPhotos()
+            val errorMessage = MutableLiveData<String>()
 
-        response.enqueue( object : Callback<PhotosJson> {
-            override fun onResponse(call: Call<PhotosJson>, response: Response<PhotosJson>) {
+            response.enqueue( object : Callback<PhotosJson> {
+                override fun onResponse(call: Call<PhotosJson>, response: Response<PhotosJson>) {
 
-                if(response.body() != null) {
-                    _allPhotosLiveDataJson.postValue(response.body())
-                    _allPhotosLiveDataList.postValue(response.body()?.photos?.photo)
+                    if(response.body() != null) {
+                        _allPhotosLiveDataJson.postValue(response.body())
+                        _allPhotosLiveDataList.postValue(response.body()?.photos?.photo)
 
-                    Log.d(TAG, "${response.body()}")
-                    Log.d(TAG, "Mapping for ${response.body()?.photos?.photo?.size} items...")
+                        Log.d(TAG, "${response.body()}")
+                        Log.d(TAG, "Mapping for ${response.body()?.photos?.photo?.size} items...")
+                    }
                 }
-            }
 
-            override fun onFailure(call: Call<PhotosJson>, t: Throwable) {
-                errorMessage.postValue(t.message)
-            }
-        })
+                override fun onFailure(call: Call<PhotosJson>, t: Throwable) {
+                    errorMessage.postValue(t.message)
+                }
+            })
+        }
     }
 
     companion object {
--- app/src/main/java/com/imawo/flickrphotos/viewmodel/SizesViewModel.kt
+++ app/src/main/java/com/imawo/flickrphotos/viewmodel/SizesViewModel.kt
@@ -2,9 +2,11 @@ package com.imawo.flickrphotos.viewmodel
 
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.imawo.flickrphotos.model.Size
 import com.imawo.flickrphotos.model.SizesJson
 import com.imawo.flickrphotos.repository.SizesRepository
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -19,25 +21,26 @@ class SizesViewModel(private val repository: SizesRepository) {
         get() = _sizesLiveDataList
 
     fun getAllSizes(photoId: String) {
+        viewModelScope.launch {
+            val response = repository.getAllSizes(photoId)
+            val errorMessage = MutableLiveData<String>()
 
-        val response = repository.getAllSizes(photoId)
-        val errorMessage = MutableLiveData<String>()
+            response.enqueue(object : Callback<SizesJson> {
+                override fun onResponse(call: Call<SizesJson>, response: Response<SizesJson>) {
 
-        response.enqueue( object : Callback<SizesJson>{
-            override fun onResponse(call: Call<SizesJson>, response: Response<SizesJson>) {
+                    if (response.body() != null) {
+                        _sizesLiveDataJson.postValue(response.body())
+                        _sizesLiveDataList.postValue(response.body()?.sizes?.size)
 
-                if(response.body() != null) {
-                    _sizesLiveDataJson.postValue(response.body())
-                    _sizesLiveDataList.postValue(response.body()?.sizes?.size)
-
-                    Log.d(TAG, "${response.body()}")
+                        Log.d(TAG, "${response.body()}")
+                    }
                 }
-            }
 
-            override fun onFailure(call: Call<SizesJson>, t: Throwable) {
-                errorMessage.postValue(t.message)
-            }
-        })
+                override fun onFailure(call: Call<SizesJson>, t: Throwable) {
+                    errorMessage.postValue(t.message)
+                }
+            })
+        }
     }
 
     companion object {
